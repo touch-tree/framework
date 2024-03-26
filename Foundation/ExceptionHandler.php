22ea37275d817a9bcd5ebe05540018092ab1dd20
echtyushi <ja.agripo2003@gmail.com>
2024-03-26 16:33:07 +0100

    (WIP) ExceptionHandler added and more things to-do

--- Foundation/ExceptionHandler.php
+++ Foundation/ExceptionHandler.php
@@ -4,15 +4,35 @@ namespace Framework\Foundation;
 
 use Exception;
 
+/**
+ * The ExceptionHandler class handles exceptions thrown within the framework.
+ *
+ * It provides a centralized place for exception handling logic.
+ *
+ * @package Framework\Foundation
+ */
 class ExceptionHandler
 {
+    /**
+     * ExceptionHandler constructor.
+     *
+     * Initializes the exception handler by setting a custom exception handler.
+     * When an exception occurs, it will be passed to the handle method of this class.
+     */
     public function __construct()
     {
-        set_exception_handler([$this, fn(Exception $e) => $this->render($e)]);
+        set_exception_handler(fn(Exception $e) => $this->handle($e));
     }
 
-    public function render()
+    /**
+     * Handle the exception.
+     *
+     * This method is called when an uncaught exception occurs within the framework.
+     * It provides a centralized location to handle exceptions and implement custom logic.
+     *
+     * @param Exception $exception The exception to handle
+     */
+    public function handle(Exception $exception)
     {
-
     }
 }
