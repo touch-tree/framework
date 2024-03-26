f81bf00b34ad8fd44d172adf4fbdd092d4ae6da3
echtyushi <ja.agripo2003@gmail.com>
2024-03-26 16:29:46 +0100

    (WIP) ExceptionHandler added and more things to-do

--- Foundation/ExceptionHandler.php
+++ Foundation/ExceptionHandler.php
@@ -2,14 +2,17 @@
 
 namespace Framework\Foundation;
 
+use Exception;
+
 class ExceptionHandler
 {
     public function __construct()
     {
-        set_exception_handler([$this, ]);
+        set_exception_handler([$this, fn(Exception $e) => $this->render($e)]);
     }
 
-    public function render() {
+    public function render()
+    {
 
     }
 }
