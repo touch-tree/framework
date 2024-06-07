4d0f393a27c2d83001d2865556ab6fb6fb7d2bad
echtyushi <ja.agripo2003@gmail.com>
2024-06-07 13:26:41 +0200

    refactor!: renamed Router to Route facade

--- Support/Facades/Route.php
+++ Support/Facades/Route.php
@@ -3,24 +3,24 @@
 namespace Framework\Support\Facades;
 
 use Framework\Routing\RouteCollection;
-use Framework\Routing\Router as ApplicationRouter;
+use Framework\Routing\Router;
 
 /**
- * Router facade.
+ * Route facade.
  *
  * @package Framework\Support\Facades
- * @see Router
+ * @see Route
  */
-class Router extends Facade
+class Route extends Facade
 {
     /**
      * Set the accessor for the facade.
      *
-     * @return ApplicationRouter
+     * @return Router
      */
     protected static function accessor(): object
     {
-        return get(ApplicationRouter::class);
+        return get(Router::class);
     }
 
     /**
@@ -28,9 +28,9 @@ class Router extends Facade
      *
      * @param string $uri The URI pattern for the route.
      * @param array|string $action An array representing the controller and method to be called for this route.
-     * @return ApplicationRouter The Router instance.
+     * @return Router The Router instance.
      */
-    public static function get(string $uri, $action): ApplicationRouter
+    public static function get(string $uri, $action): Router
     {
         return self::accessor()->get($uri, $action);
     }
@@ -40,9 +40,9 @@ class Router extends Facade
      *
      * @param string $uri The URI pattern for the route.
      * @param array $action An array representing the controller and method to be called for this route.
-     * @return ApplicationRouter The Router instance.
+     * @return Router The Router instance.
      */
-    public static function post(string $uri, array $action): ApplicationRouter
+    public static function post(string $uri, array $action): Router
     {
         return self::accessor()->post($uri, $action);
     }
